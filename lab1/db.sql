e2706f8e498744476bea65a64a52b00bf7e520cb
Khalim Khamzin <twoxowt@yandex.ru>
2020-02-20 14:54:42 +0300

    .

--- lab1/db.sql
+++ lab1/db.sql
@@ -23,54 +23,54 @@ drop table if exists worker;
 CREATE TABLE Bill
 ( 
 	id_bill              serial primary key,
-	Ammount              money  NOT NULL 
+	ammount              money  NOT NULL 
 );
 
 CREATE TABLE Build
 ( 
 	id_Build             serial primary key,
-	Build_adress         varchar(45)  NOT NULL ,
-	Prestige             integer  NOT NULL ,
-	Build_number         integer  NOT NULL 
+	build_adress         varchar(45)  NOT NULL ,
+	prestige             integer  NOT NULL ,
+	build_number         integer  NOT NULL 
 );
 
 CREATE TABLE Client
 ( 
 	id_client            serial primary key,
-	Adress               varchar(45)  NOT NULL ,
-	Phone_number         varchar(11)  NOT NULL 
+	adress               varchar(45)  NOT NULL ,
+	phone_number         varchar(11)  NOT NULL 
 );
 
 CREATE TABLE Disscount
 ( 
 	id_disscount         serial primary key,
-	Disscount_name       varchar(64)  NOT NULL ,
-	Diss_person          decimal  NULL ,
-	Diss_legal           decimal  NULL 
+	disscount_name       varchar(64)  NOT NULL ,
+	diss_person          decimal  NULL ,
+	diss_legal           decimal  NULL 
 );
 
 CREATE TABLE FeedBack
 ( 
 	id_feed_back         serial primary key,
 	id_client            integer  references client(id_client),
-	Mark                 integer  NULL ,
-	Descroption          varchar(512)  NULL 
+	mark                 integer  NULL ,
+	descroption          varchar(512)  NULL 
 );
 
 CREATE TABLE Fiz_person
 ( 
 	id_client            integer references client(id_client),
-	Surname              varchar(20)  NOT NULL ,
-	Name                 varchar(20)  NOT NULL ,
-	LastName             varchar(20)  NOT NULL ,
-	Birth_date           timestamp  NOT NULL ,
-	Passport             varchar(10)  NOT NULL 
+	surname              varchar(20)  NOT NULL ,
+	forename                 varchar(20)  NOT NULL ,
+	lastName             varchar(20)  NOT NULL ,
+	birth_date           timestamp  NOT NULL ,
+	passport             varchar(10)  NOT NULL 
 );
 
 CREATE TABLE Include_service
 ( 
 	id_service           serial primary key,
-	Service_name         varchar(64)  NOT NULL ,
+	service_name         varchar(64)  NOT NULL ,
 	price                 money  NOT NULL 
 );
 
@@ -78,64 +78,64 @@ CREATE TABLE Optional_service
 ( 
 	id_option_service    serial primary key,
 	option_service_name  varchar(64)  NOT NULL ,
-	Option_cost          money  NOT NULL 
+	option_cost          money  NOT NULL 
 );
 
 CREATE TABLE Room
 ( 
 	id_room              serial primary key,
-	Beds_count           integer  NOT NULL ,
+	beds_count           integer  NOT NULL ,
 	floor_lvl            integer  NOT NULL ,
-	Room_number          integer  NOT NULL ,
-	Room_cost            money  NOT NULL ,
+	room_number          integer  NOT NULL ,
+	room_cost            money  NOT NULL ,
 	id_Build             integer  references build(id_build) 
 );
 
 CREATE TABLE Status_booking
 ( 
-	Id_status            serial primary key,
-	Name_status          varchar(64)  NOT NULL 
+	id_status            serial primary key,
+	name_status          varchar(64)  NOT NULL 
 );
 
 CREATE TABLE UR_Person
 ( 
 	id_client            integer  references client(id_client),
-	Personal__account    varchar(20)  NULL ,
-	ITN                  varchar(20)  NOT NULL ,
-	UPRLP                varchar(20)  NOT NULL ,
-	Name_organization    varchar(64)  NOT NULL 
+	personal__account    varchar(20)  NULL ,
+	itn                  varchar(20)  NOT NULL ,
+	uprlp                varchar(20)  NOT NULL ,
+	name_organization    varchar(64)  NOT NULL 
 );
 
 CREATE TABLE Worker
 ( 
 	id_worker            serial primary key,
-	Surname              varchar(20)  NOT NULL ,
-	Forename             varchar(20)  NOT NULL ,
-	Lastname             varchar(20)  NULL ,
-	id_parent            integer  NOT NULL ,
-	Employment_number    integer  NOT NULL ,
-	Birth_date           timestamp NOT NULL ,
-	Passport             varchar(10)  NOT NULL ,
-	Address              varchar(64)  NOT NULL ,
-	ITN                  varchar(64)  NOT NULL 
+	surname              varchar(20)  NOT NULL ,
+	sorename             varchar(20)  NOT NULL ,
+	lastname             varchar(20)  NULL ,
+	id_parent            integer  references worker(id_worker),
+	employment_number    integer  NOT NULL ,
+	birth_date           timestamp NOT NULL ,
+	passport             varchar(10)  NOT NULL ,
+	address              varchar(64)  NOT NULL ,
+	itn                  varchar(64)  NOT NULL 
 );
 
 CREATE TABLE Bilt_position
 ( 
-	Id_bilt_position     serial primary key,
+	id_bilt_position     serial primary key,
 	id_bill              integer  references  bill(id_bill) ,
 	id_disscount         integer  references disscount(id_disscount) ,
 	id_option_service    integer  references optional_service(id_option_service) ,
-	Without_VAT          money  NOT NULL ,
-	Without_disscount    money  NOT NULL 
+	without_VAT          money  NOT NULL ,
+	without_disscount    money  NOT NULL 
 );
 
 CREATE TABLE Contract
 ( 
 	Id_contract          serial primary key,
-	Contract_number      integer  NOT NULL ,
-	Start_date           timestamp  NOT NULL ,
-	End_date             timestamp  NOT NULL ,
+	contract_number      integer  NOT NULL ,
+	start_date           timestamp  NOT NULL ,
+	end_date             timestamp  NOT NULL ,
 	id_client            integer  references client(id_client) ,
 	id_bill              integer  references bill(id_bill),
 	id_worker			 integer references worker(id_worker)
@@ -150,11 +150,11 @@ CREATE TABLE Include_service_number
 CREATE TABLE Booking
 ( 
 	id_booking           serial primary key,
-	Id_contract          integer  references contract(id_contract) ,
-	Id_status            integer  references status_booking(id_status),
-	Settlement_time      timestamp  NOT NULL ,
-	Departure_time       timestamp  NOT NULL ,
-	Booking_time         timestamp  NOT NULL ,
+	id_contract          integer  references contract(id_contract) ,
+	id_status            integer  references status_booking(id_status),
+	settlement_time      timestamp  NOT NULL ,
+	departure_time       timestamp  NOT NULL ,
+	booking_time         timestamp  NOT NULL ,
 	id_room              integer  references room(id_room)
 );
 
